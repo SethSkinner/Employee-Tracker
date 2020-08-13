a9de3b7f052d2eab3d9bd2c0e44b9de2b133454e
seth skinner <sethwskinner@gmail.com>
2020-08-13 15:02:08 -0500

    adding basic search functions

--- app.js
+++ app.js
@@ -18,6 +18,82 @@ connection.connect(function(err) {
 function runSearch() {
     inquirer
         .prompt({
-            name:
+            name: 'action',
+            type: 'rawlist',
+            message: 'what would you like to do?',
+            choices: [
+                'view employee list',
+                'view department list',
+                'view employee roles',
+                'add new employee',
+                'add new department',
+                'add new employee role',
+                'update employee role',
+                'remove current employee',
+                'exit employee tracker'
+            ]
         })
-}
+        .then(function(answer){
+            switch(answer.action) {
+                case 'view employee list':
+                    viewEmployeeList();
+                    break;
+
+                case 'view department list':
+                    viewDepartmentList();
+                    break;
+
+                case 'view employee roles':
+                    viewEmployeeRole();
+                    break;
+
+                case 'add new employee':
+                    addNewEmployee();
+                    break;
+
+                case 'add new department':
+                    addNewDepartment();
+                    break;
+
+                case 'add new employee role':
+                    addNewEmployeeRole();
+                    break;
+
+                case 'update employee role':
+                    updateEmployeeRole();
+                    break;
+
+                case 'remove current employee':
+                    removeCurrentEmployee();
+                    break;
+
+                case 'exit employee tracker':
+                    exitEmployeeTracker();
+                    break;
+            }
+        });
+};
+
+function viewEmployeeList() {
+   connection.query('SELECT * FROM employee', function(err, res) {
+        if (err) throw err;
+            console.table(res);
+                runSearch();
+   }
+)};
+
+function viewDepartmentList() {
+    connection.query('SELECT * FROM departments', function(err,res) {
+        if (err) throw err;
+            console.table(res);
+                runSearch();
+    }
+)};
+
+function viewEmployeeRole() {
+    connection.query('SELECT * FROM roles', function(err,res){
+        if (err) throw err;
+            console.table(res);
+                runSearch();
+    }
+)};
